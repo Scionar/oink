ea8cf98a47c3e2b9e1c6d5da90cb7a7dd1c30624
Joona Viertola <joona.viertola@houston-inc.com>
2023-02-19 13:59:02 +0200

    Create AppTitle component

---
+++ packages/ui/components/AppTitle/AppTitle.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import styles from "./AppTitle.module.css";
+
+export const AppTitle = () => (
+  <div className={styles.AppTitle}>
+    <div className={styles.stage}>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+      <div className={styles.layer}></div>
+    </div>
+  </div>
+);
