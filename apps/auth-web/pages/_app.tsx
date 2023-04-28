186cb0c32e8759abd78cd9a3dab1ec7da7dcfa05
Joona Viertola <joona.viertola@houston-inc.com>
2023-04-28 12:39:53 +0300

    Add new app for login

---
+++ apps/auth-web/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "ui/normalize.css";
+import "ui/global.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="mask-icon" href="favicon.svg" color="#000000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ apps/auth-web/pages/index.tsx
@@ -0,0 +1,73 @@
+import Head from "next/head";
+import { Article, Button, Input, Panel, Snout, Spacer } from "ui";
+import { ChangeEvent, useState } from "react";
+import Link from "next/link";
+
+export default function Home() {
+  const [emailValue, setEmailValue] = useState<string>("");
+
+  const emailOnChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmailValue(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Oink Authentication</title>
+      </Head>
+      <Article>
+        <Spacer>
+          <form onSubmit={onSubmitHandler}>
+            <Spacer variant="form">
+              <Snout style={{ alignSelf: "center" }} />
+
+              <Spacer variant="article">
+                <Input
+                  label="Email"
+                  value={emailValue}
+                  name="email"
+                  required
+                  onChange={emailOnChangeHandler}
+                />
+
+                <Input
+                  label="Password"
+                  value={emailValue}
+                  name="password"
+                  required
+                  onChange={emailOnChangeHandler}
+                />
+
+                <Button variant="positive">Login</Button>
+              </Spacer>
+            </Spacer>
+          </form>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <p>Don't have an account yet?</p>
+
+            <p>
+              <Link
+                href="/sign-up"
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </Spacer>
+      </Article>
+    </>
+  );
+}
