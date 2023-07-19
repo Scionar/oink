c79250711ff4886248c483de7c537279336c9496
Joona Viertola <git@joonaviertola.com>
2023-07-19 14:28:32 +0300

    Improve linting and add husky

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "prettier"
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/no-unescaped-entities": ["error", { forbid: [">", '"', "}"] }],
